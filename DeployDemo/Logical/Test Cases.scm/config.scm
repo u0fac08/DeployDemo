fe4f961a0fb7a438f27360fe36ac078b8dd37216
Mark garcia <msgarcia@us.ibm.com>
2025-03-04 14:01:17 -0500

    Added LoginTC200

---
+++ DeployDemo/Logical/Test Cases.scm/config.scm
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<editableResource createdTimestamp="1741114824204" createdUser="Mark"
+    id="23af0489:1954ca57c7d:-4cd5" type="service_component_resource"
+    updatedTimestamp="1741114824204" updatedUser="Mark" version="1.1">
+    <loggingConfiguration debug="false" resultWriterLoggingLevel="FULL" writeStubEventsToDB="false"/>
+</editableResource>
